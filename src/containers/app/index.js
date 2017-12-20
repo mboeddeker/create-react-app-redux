5b71fd2718f2784bb12f84f64074fab46cb19ca8
mboeddeker <marvin.boeddeker@gmail.com>
2017-12-20 13:46:44 +0100

    Anpassungen

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom'
+import { Grid, Row, Col, Button, Nav, Navbar, NavItem} from 'react-bootstrap';
 import Home from '../home'
 import About from '../about'
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-    </header>
-
+    <Navbar inverse staticTop>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">React-Redux-Starter</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem><Link to="/">Home</Link></NavItem>
+          <NavItem><Link to="/about-us">About</Link></NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+      
+    <div className="container">
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
     </main>
+    </div>
+
   </div>
 )
 
